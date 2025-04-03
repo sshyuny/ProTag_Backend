52ddb4526355be043dc882fb73b5d78d6e3d0814
sshyuny <thgustls@gmail.com>
2025-04-03 20:45:28 +0900

    fix: etc

--- src/main/java/com/sshyu/protag/configure/MySqlMyBatisDatabaseConfig.java
+++ src/main/java/com/sshyu/protag/configure/MySqlMyBatisDatabaseConfig.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,6 +11,9 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
 
 @Configuration
 @MapperScan(
@@ -40,9 +42,9 @@ public class MySqlMyBatisDatabaseConfig {
     }
 
     @Bean
-    public SqlSessionTemplate mybatisSqlSessionTemplate(
-        @Qualifier("mybatisSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
+    public PlatformTransactionManager mySqlMyBatisTransactionManager(
+            @Qualifier("mySqlMybatisDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
     
 }
--- src/test/java/com/sshyu/protag/adapter/out/persistence/mysql/project/ProjectRepositoryAdapterTest.java
+++ src/test/java/com/sshyu/protag/adapter/out/persistence/mysql/project/ProjectRepositoryAdapterTest.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.sshyu.protag.domain.project.model.Project;
 
-// @ActiveProfiles("test")
+@ActiveProfiles("test")
 @SpringBootTest
 public class ProjectRepositoryAdapterTest {
     
